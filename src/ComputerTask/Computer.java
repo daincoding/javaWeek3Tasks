12e8a76b1913afb13c5e867f8b0da3425deee4cf
daincoding <official.toyou@gmail.com>
2025-06-08 18:25:27 +0200

    Created all Constructors, Getters & Setters

--- src/ComputerTask/Computer.java
+++ src/ComputerTask/Computer.java
@@ -1,21 +1,124 @@
 package ComputerTask;
 
 public class Computer {
+    //region Variables
+    // Private fields
     private String manufacturer;
-    private double mhz;
-    private int ram;
-    private int hardDrive;
-    private double screenSize;
+    private int processorSpeedMHz;
+    private int ramGB;
+    private int hardDriveGB;
+    private int usbPorts;
+    private double screenSizeInches;
     private String computerType;
-    private boolean hasOperatingSystem;
+    private boolean hasOS;
 
-    public Computer(String manufacturer, double mhz, int ram, int hardDrive, double screenSize, String computerType, boolean hasOperatingSystem) {
-        this.manufacturer = manufacturer;
-        this.mhz = mhz;
-        this.ram = ram;
-        this.hardDrive = hardDrive;
-        this.screenSize = screenSize;
-        this.computerType = computerType;
-        this.hasOperatingSystem = hasOperatingSystem;
+    //endregion
+
+    // region  Constructors
+    // 1) Default constructor
+    public Computer() {
+        this.manufacturer = "Unknown";
+        this.processorSpeedMHz = 0;
+        this.ramGB = 0;
+        this.hardDriveGB = 0;
+        this.usbPorts = 0;
+        this.screenSizeInches = 0.0;
+        this.computerType = "Unknown";
+        this.hasOS = false;
     }
+
+    // 2) Constructor with minimum values
+    public Computer(String manufacturer, double screenSizeInches, boolean hasOS) {
+        this.manufacturer = manufacturer;
+        this.screenSizeInches = screenSizeInches;
+        this.hasOS = hasOS;
+        this.processorSpeedMHz = 0;
+        this.ramGB = 0;
+        this.hardDriveGB = 0;
+        this.usbPorts = 0;
+        this.computerType = "Unknown";
+    }
+
+    // 3) Constructor with all values
+    public Computer(String manufacturer, int processorSpeedMHz, int ramGB, int hardDriveGB,
+                    int usbPorts, double screenSizeInches, String computerType, boolean hasOS) {
+        this.manufacturer = manufacturer;
+        this.processorSpeedMHz = processorSpeedMHz;
+        this.ramGB = ramGB;
+        this.hardDriveGB = hardDriveGB;
+        this.usbPorts = usbPorts;
+        this.screenSizeInches = screenSizeInches;
+        this.computerType = computerType;
+        this.hasOS = hasOS;
+    }
+
+    //endregion
+
+    //region Getters & Setters
+    // Getters and setters
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getProcessorSpeedMHz() {
+        return processorSpeedMHz;
+    }
+
+    public void setProcessorSpeedMHz(int processorSpeedMHz) {
+        this.processorSpeedMHz = processorSpeedMHz;
+    }
+
+    public int getRamGB() {
+        return ramGB;
+    }
+
+    public void setRamGB(int ramGB) {
+        this.ramGB = ramGB;
+    }
+
+    public int getHardDriveGB() {
+        return hardDriveGB;
+    }
+
+    public void setHardDriveGB(int hardDriveGB) {
+        this.hardDriveGB = hardDriveGB;
+    }
+
+    public int getUsbPorts() {
+        return usbPorts;
+    }
+
+    public void setUsbPorts(int usbPorts) {
+        this.usbPorts = usbPorts;
+    }
+
+    public double getScreenSizeInches() {
+        return screenSizeInches;
+    }
+
+    public void setScreenSizeInches(double screenSizeInches) {
+        this.screenSizeInches = screenSizeInches;
+    }
+
+    public String getComputerType() {
+        return computerType;
+    }
+
+    public void setComputerType(String computerType) {
+        this.computerType = computerType;
+    }
+
+    public boolean hasOS() {
+        return hasOS;
+    }
+
+    public void setHasOS(boolean hasOS) {
+        this.hasOS = hasOS;
+    }
+
+    //endregion
 }
